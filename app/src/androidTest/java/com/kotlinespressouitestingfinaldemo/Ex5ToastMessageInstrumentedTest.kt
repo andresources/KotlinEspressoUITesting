9c955efed4b398d02143b041fb4d33457ff93541
Hari <andresources1039@gmail.com>
2023-04-11 19:11:47 +0530

    Toast checking

--- app/src/androidTest/java/com/kotlinespressouitestingfinaldemo/Ex5ToastMessageInstrumentedTest.kt
+++ app/src/androidTest/java/com/kotlinespressouitestingfinaldemo/Ex5ToastMessageInstrumentedTest.kt
@@ -1,18 +1,50 @@
 package com.kotlinespressouitestingfinaldemo
 
-import androidx.test.espresso.Espresso
+import android.os.IBinder
+import android.view.WindowManager
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Root
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.RootMatchers.withDecorView
 import androidx.test.espresso.matcher.ViewMatchers.*
-import org.hamcrest.Matchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.hamcrest.Description
 import org.hamcrest.Matchers.not
+import org.hamcrest.TypeSafeMatcher
 import org.junit.Rule
 import org.junit.Test
 
 
 class Ex5ToastMessageInstrumentedTest {
+    @get:Rule
+    var activityScenarioRule = ActivityScenarioRule(Ex5_ToastMessageActivity::class.java)
+    @Test
+    fun testToast(){
+        onView(withId(R.id.btnClickMe)).perform(click())
+        onView(withText("This is toast"))
+            .inRoot(ToastMatcher().apply {
+                matches(isDisplayed())
+                //matches(not(isDisplayed())) Incase not equal to
+            })
 
+    }
+    class ToastMatcher : TypeSafeMatcher<Root?>() {
+
+        override fun matchesSafely(item: Root?): Boolean {
+            val type: Int? = item?.windowLayoutParams?.get()?.type
+            if (type == WindowManager.LayoutParams.FIRST_APPLICATION_WINDOW) {
+                val windowToken: IBinder = item.decorView.windowToken
+                val appToken: IBinder = item.decorView.applicationWindowToken
+                if (windowToken === appToken) { // means this window isn't contained by any other windows.
+                    return true
+                }
+            }
+            return false
+        }
+
+        override fun describeTo(description: Description?) {
+            description?.appendText("is toast")
+        }
+    }
 
 }
